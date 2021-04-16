7b630c72f3c16f98163570f242a99dccacd9a4b9
Jussi Viitamäki <jussi@kwd.fi>
2021-04-16 09:00:26 +0300

    ST-14: Added support for elasticsearch relationships in platform

--- public/sites/default/settings.php
+++ public/sites/default/settings.php
@@ -90,9 +90,17 @@ if ($env = getenv('APP_ENV')) {
   if (file_exists(__DIR__ . '/local.services.yml')) {
     $settings['container_yamls'][] = __DIR__ . '/local.services.yml';
   }
+  $settings['container_yamls'][] = DRUPAL_ROOT . '/sites/development.services.yml';
 
   if (file_exists(__DIR__ . '/local.settings.php')) {
     include __DIR__ . '/local.settings.php';
   }
 }
 
+if (getenv('PLATFORM_RELATIONSHIPS')) {
+  $relationships = json_decode(base64_decode(getenv('PLATFORM_RELATIONSHIPS')), TRUE);
+  if (!empty($relationships['elasticsearch'])) {
+    $elasticsearch = $relationships['elasticsearch'];
+    $config['elasticsearch_connector.cluster.search']['url'] = $elasticsearch[0]['host'];
+  }
+}
