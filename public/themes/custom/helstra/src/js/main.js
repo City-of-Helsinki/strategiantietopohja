ce1e481461f916e8524db44f39a794e83d6b505d
veikkoa <veikko.aitosalo@citrus.fi>
2021-06-07 10:32:19 +0300

    ST-44: Improvements to article TOC

--- public/themes/custom/helstra/src/js/main.js
+++ public/themes/custom/helstra/src/js/main.js
@@ -13,21 +13,28 @@
     // ------------
     mobileTOC: function() {
       setTimeout(function() {
-        $('.article-table-of-contents').css("margin-bottom", (-64 - $('.article-table-of-contents').height()) + "px");
+        $('.article-table-of-contents').css("margin-bottom", -$('.article-table-of-contents').height() + "px");
       }, 500);
   
-      $('.article__toc button').on("click", function () {
-        $('.article-table-of-contents').toggleClass("open");
-        $(this).blur();
+      $('.article__toc button').on("click", function() {
+        if ($(this).hasClass('focused')) {
+          $(this).removeClass('focused');
+          $(this).blur();
+        } else {
+          $(this).addClass('focused');
+        }
+      });
+
+      $('.article__toc button').focusout(function() {
+        $(this).removeClass('focused');
       });
 
       $(window).on("load resize", function() {
         if ($(window).outerWidth() < Drupal.behaviors.helstra_theme.breakpoints['md']) {
-          $('.article-table-of-contents').css("margin-bottom", (-64 - $('.article-table-of-contents').height()) + "px");
+          $('.article-table-of-contents').css("margin-bottom", -$('.article-table-of-contents').height() + "px");
           $('.article__toc').addClass("sticky");
         } else {
           $('.article__toc').removeClass("sticky ");
-          $('.article-table-of-contents').removeClass("open");
         }
       });
     },
