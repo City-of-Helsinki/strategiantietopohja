08bc7420a8f8d569383bca5a85a2a7c2a1fd8323
veikkoa <veikko.aitosalo@citrus.fi>
2021-06-10 12:31:04 +0300

    ST-44: Fix mobile TOC .focused bug

--- public/themes/custom/helstra/src/js/main.js
+++ public/themes/custom/helstra/src/js/main.js
@@ -12,23 +12,36 @@
     // Sticky mobile TOC
     // ------------
     mobileTOC: function() {
-      if ($('#article-toc').length){
+      if ($('#article-toc').length) {
 
-        setTimeout(function() {
+        if ($(window).outerWidth() < Drupal.behaviors.helstra_theme.breakpoints['md']) {
           $('.article-table-of-contents').css("margin-bottom", -$('.article-table-of-contents').height() + "px");
-        }, 500);
-    
-        $('.article__toc button').on("click", function() {
+          $('.article__toc').addClass("sticky");
+        } 
+
+        $('.article__toc button').on('mousedown', function(event) {
+          event.preventDefault();
           if ($(this).hasClass('focused')) {
             $(this).removeClass('focused');
-            $(this).blur();
+            $(':focus').blur();
           } else {
             $(this).addClass('focused');
+            $(this).focus();
           }
         });
 
-        $('.article__toc button').focusout(function() {
-          $(this).removeClass('focused');
+        $('.article__toc button').on('blur', function(event) {
+          if ($(this).hasClass('focused')) {
+            $(this).removeClass('focused');
+          }
+        });
+
+        $('#article-toc').on('focusout', function() {
+          $('.article__toc button').removeClass('focused');
+        });
+
+        $('#article-toc').on('focusin', function() {
+          $('.article__toc button').addClass('focused');
         });
 
         $(window).on("load resize", function() {
