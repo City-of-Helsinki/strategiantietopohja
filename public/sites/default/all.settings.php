f1fd2e08bf9d6b17a01217d84353795540d78f7a
veikkoa <veikko.aitosalo@citrus.fi>
2023-12-19 14:07:33 +0200

    HELS-8: Add elasticsearch settings

--- public/sites/default/all.settings.php
+++ public/sites/default/all.settings.php
@@ -4,3 +4,11 @@
  * @file
  * Contains site specific overrides.
  */
+
+// Elasticsearch settings.
+if ($env = getenv('ELASTICSEARCH_URL')) {
+  $config['elasticsearch_connector.cluster.local']['url'] = getenv('ELASTICSEARCH_URL');
+  $config['elasticsearch_connector.cluster.local']['options']['use_authentication'] = '1';
+  $config['elasticsearch_connector.cluster.local']['options']['username'] = getenv('ELASTIC_USER');
+  $config['elasticsearch_connector.cluster.local']['options']['password'] = getenv('ELASTIC_PASSWORD');
+}
